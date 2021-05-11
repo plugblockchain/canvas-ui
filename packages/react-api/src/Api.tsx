e5c39786a2ec977e755408d9076c144ffb7fb607
aliXSed <alex.sedighi@centrality.ai>
2021-05-11 15:38:44 +1200

    Revert "Use CENNZnet and CENNZnet 2.0 types"
    This reverts commit 054a45494e3a65622562ead1b65ab0aea3025f44.

--- packages/react-api/src/Api.tsx
+++ packages/react-api/src/Api.tsx
@@ -8,8 +8,6 @@ import type { ChainProperties, ChainType } from '@polkadot/types/interfaces';
 import type { KeyringStore } from '@polkadot/ui-keyring/types';
 import type { ApiProps, ApiState } from './types';
 
-import cennznetTypes from '@cennznet/types/interfaces/injects';
-
 import { typesChain, typesSpec } from '@canvas-ui/app-config/api';
 import React, { useContext, useEffect, useMemo, useState } from 'react';
 
@@ -179,44 +177,7 @@ function Api ({ children, store, url }: Props): React.ReactElement<Props> | null
     const provider = new WsProvider(url);
     const signer = new ApiSigner(queuePayload, queueSetTxStatus);
 
-    const cennznet2ExtraTypes = {
-      BlockWeights: {
-        base_block: 'Weight',
-        max_block: 'Weight',
-        per_class: 'PerDispatchClass1'
-      },
-      PerDispatchClass1: {
-        normal: 'WeightsPerClass',
-        operational: 'WeightsPerClass',
-        mandatory: 'WeightsPerClass'
-      },
-      PerDispatchClass2: {
-        normal: 'u32',
-        operational: 'u32',
-        mandatory: 'u32'
-      },
-      ConsumedWeight: {
-        normal: 'Weight',
-        operational: 'Weight',
-        mandatory: 'Weight'
-      },
-      WeightsPerClass: {
-        base_extrinsic: 'Weight',
-        max_extrinsic: 'Option<Weight>',
-        max_total: 'Option<Weight>',
-        reserved: 'Option<Weight>'
-      },
-      BlockLength: {
-        max: 'PerDispatchClass2'
-      },
-      DeletedContract: {
-        pair_count: 'u32',
-        trie_id: 'TrieId'
-      },
-      Slot: 'u64'
-    };
-
-    api = new ApiPromise({ provider, registry, signer, typesChain, typesSpec, types: { ...cennznetTypes, ...cennznet2ExtraTypes } });
+    api = new ApiPromise({ provider, registry, signer, typesChain, typesSpec });
 
     api.on('connected', () => setIsApiConnected(true));
     api.on('disconnected', () => setIsApiConnected(false));
